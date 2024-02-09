2fbcf19e50ab47e3a07138e8b22f1ec0981597db
amunir404 <amunir.developer@gmail.com>
2024-02-10 02:17:24 +0700

    kedua

--- core/settings.py
+++ core/settings.py
@@ -96,10 +96,10 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "b_ptps",
-        "USER": "postgres",
-        "PASSWORD": "123456",
-        "HOST": "localhost",
+        "NAME": "db_ptps",
+        "USER": "amunir404",
+        "PASSWORD": "Karawang45",
+        "HOST": "ptpsinstance.c9uy2i8cmsm1.ap-southeast-1.rds.amazonaws.com",
         "PORT": "5432",
     }
 }
@@ -191,3 +191,13 @@ CAPTCHA_LENGTH = 5
 CAPTCHA_IMAGE_SIZE = (150, 50)
 
 LOGIN_REDIRECT_URL = "/"
+
+AWS_ACCESS_KEY_ID = 'AKIAZI2LCEBRHHQCG5I5'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'ptps'
+AWS_S3_SIGNATURE_NAME = "s3v3"
+AWS_S3_REGION_NAME = 'ap-southeast-1'
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+AWS_S3_VERITY = True
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
